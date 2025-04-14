5ace1dc89a0f73e49122e61e9bd4a71ce84a64b5
zastv <132970150+zastv@users.noreply.github.com>
2025-04-14 15:59:37 -0600

    actualizacion

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3001;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3001',
   credentials: true
 }));
 
